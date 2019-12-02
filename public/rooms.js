350279b96274517b2414ff26de7d30c977c54eb6
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2019-12-02 14:12:55 -0600

    Adreenas exercise

--- public/rooms.js
+++ public/rooms.js
@@ -2,7 +2,7 @@ $(function() {
   $("#responsesection, #newqueryheading").hide();
   $("form").on("submit", function() {
     var query = $("#query").val();
-    $.get("/api/whereis", { q: query }, function(data) {
+    $.get("/api/locations", { q: query }, function(data) {
       $("#previousquery").text(query);
       $("#response").text(query + " is located " + data);
       $("#responsesection, #newqueryheading").show();
